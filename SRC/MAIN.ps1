5af77cc3d5caf85ba97f4fdc0db8d45f7bb8f02f
GoHo-DohJi <3dzhio7@gmail.com>
2026-03-31 09:25:53 +0200

    Add files via upload

---
+++ SRC/MAIN.ps1
@@ -0,0 +1,54 @@
+﻿### IMPORT ### 
+. "$PSScriptRoot\utils.ps1"
+. "$PSScriptRoot\VARIABLES.ps1"
+
+
+
+### MAIN ###
+## OFFICE CHECK ##
+if (Test-Path "$OFFICE_DIR") {
+    if (-not (Write-Centered -Text "$BANNER_REINSTALL" -TextColor "DarkYellow" -Prompt_No)) { exit 0 }
+}
+
+
+## GET IMAGE PATH ##
+while ($true) {
+    Write-Centered -Text "$BANNER_GET_IMAGE_PATH" -DownloadPrompt
+
+    $IMAGE_PATH = Get-Image-Path
+    
+    if ($IMAGE_PATH) { break }
+}
+
+
+## PRODUCT SELECTOR ##
+$script:ExcludedProducts = Write-Centered -Text "$BANNER_PRODUCT_SELECTOR" -ProductSelector
+## CONFIGURATION ## 
+[xml]$xml = Get-Content "$CONFIGURATION_TEMPLATE_PATH" -Encoding "UTF8"
+$ExcludedProducts.ForEach({ $xml.Configuration.Add.Product.InnerXml += "<ExcludeApp ID=`"$($_.ID)`"/>" })
+$xml.Save($CONFIGURATION_PATH)
+
+
+## SETUP ##
+Write-Centered -Text "$BANNER_SETUP_OFFICE"
+
+try {
+    $DRIVE_LETTER = (Mount-DiskImage -ImagePath "$IMAGE_PATH" | Get-Volume).DriveLetter                          # IMAGE MOUNT #
+    # if (-not (Test-Path "${DriveLetter}:\Office\Setup64.exe")) {
+    #     Write-Centered -Text "❌ ⚠ NOT A VALID OFFICE IMAGE!"
+    #     Dismount-DiskImage -ImagePath $IMAGE_PATH
+    #     continue
+    # }
+    Start-Process "${DRIVE_LETTER}:\Office\Setup64.exe" -ArgumentList "/configure `"$CONFIGURATION_PATH`"" -Wait # SETUP #
+    & ([ScriptBlock]::Create((irm https://get.activated.win))) /para /Ohook                                      # ACTIVATION #
+    Dismount-DiskImage -ImagePath "$IMAGE_PATH"                                                                  # IMAGE DISMOUNT #
+    
+    Write-Centered -Text "$BANNER_SETUP_SUCCESS"
+} catch {
+    Dismount-DiskImage -ImagePath "$IMAGE_PATH" 
+
+    $errorText = "$BANNER_SETUP_ERROR" + "$($_.Exception.Message)"
+    Write-Centered -Text "$errorText"
+}
+
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
---
+++ SRC/VARIABLES.ps1
@@ -0,0 +1,63 @@
+﻿### CONST ###
+$OFFICE_DIR                  = "$env:PROGRAMFILES\Microsoft Office"
+$CONFIGURATION_TEMPLATE_PATH = "$PSScriptRoot\CONFIGURATION_TEMPLATE.xml"
+$CONFIGURATION_PATH          = "$PSScriptRoot\CONFIGURATION.xml"
+$DOWNLOAD_URL                = "https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/en-us/ProPlus2024Retail.img"
+
+
+
+# Fraktur Text Art ( https://fontgenro.com/ascii-text-generator ) #
+$LOGO = @"
+                                   .                         
+               oec :     oec :    @88>                       
+       u.     @88888    @88888    %8P                        
+ ...ue888b    8"*88%    8"*88%     .          .        .u    
+ 888R Y888r   8b.       8b.      .@88u   .udR88N    ud8888.  
+ 888R I888>  u888888>  u888888> '"888E` <888'888k :888'8888. 
+ 888R I888>   8888R     8888R     888E  9888 'Y"  d888 '88%" 
+ 888R I888>   8888P     8888P     888E  9888      8888.+"    
+u8888cJ888    *888>     *888>     888E  9888      8888L      
+ "*888*P"     4888      4888      888&  ?8888u../ '8888c. .+ 
+   'Y"        '888      '888      R888"  "8888P'   "88888%   
+               88R       88R       ""      "P'       "YP'    
+               88>       88>                                 
+               48        48                                  
+               '8        '8                                                          
+"@
+
+$BANNER_REINSTALL = @"
+⚠️ REINSTALL OFFICE ???
+( office is already installed )
+"@
+
+$BANNER_GET_IMAGE_PATH = @"
+📥 PRESS [D] TO START DOWNLOADING IN BROWSER OR DO IT MANUALLY USING THE LINK:
+
+$DOWNLOAD_URL
+
+💿 THEN PRESS [ENTER] TO SELECT OFFICE IMAGE FILE
+"@
+
+$BANNER_PRODUCT_SELECTOR = @"
+📦 CUSTOMIZE INSTALLATION
+( select products to install )
+"@
+
+$BANNER_SETUP_OFFICE = @"
+⚙️ SETUP OFFICE
+
+DON'T OPEN APPS & DON'T CLOSE THIS WINDOW !!!
+"@
+
+$BANNER_SETUP_SUCCESS = @"
+✅ SUCCESS
+( office installed & activated )
+
+PRESS ANY KEY TO EXIT...
+"@
+
+$BANNER_SETUP_ERROR = @"
+❌ ERROR
+
+PRESS ANY KEY TO EXIT...
+"@

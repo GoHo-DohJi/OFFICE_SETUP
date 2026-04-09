4b9b790db95b45fb1fa2c821a802a6c94bcbd301
GoHo-DohJi <3dzhio7@gmail.com>
2026-04-09 19:24:44 +0200

    Add files via upload

---
+++ INSTALL.cmd
@@ -0,0 +1,38 @@
+:: SETTINGS :: 
+@echo off
+
+
+:: ADMIN CHECK ::
+net session >nul 2>&1 || ( PowerShell -Command "Start-Process \"%~f0\" -Verb RunAs" & exit /B )
+
+
+:: VARIABLES ::
+set "REPO=OFFICE_SETUP"
+set "URL=https://github.com/GoHo-DohJi/%REPO%/archive/refs/heads/main.zip"
+set "TEMP_DIR=%TEMP%"
+set "ZIP=%TEMP_DIR%\%REPO%.zip"
+set "SCRIPT=%TEMP_DIR%\%REPO%-main\SRC\MAIN.ps1"
+
+
+:: DOWNLOAD ::
+cls
+echo [*] DOWNLOADING...
+curl.exe -sSL "%URL%" -o "%ZIP%" || goto :ERROR
+
+
+:: EXTRACT ::
+echo [*] EXTRACTING...
+tar -xf "%ZIP%" -C "%TEMP_DIR%" || goto :ERROR
+del /F /Q "%ZIP%"
+
+:: RUN ::
+echo [√] SUCCESS
+PowerShell -NoProfile -ExecutionPolicy "Bypass" -WindowStyle "Maximized" -File "%SCRIPT%"
+exit /B 0
+
+
+:ERROR
+color 4F
+echo [X] ERROR OCCURRED
+pause
+exit /B 1

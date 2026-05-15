7c4597f44c9762b69ca153663dfe36516d58d3c8
GoHo-DohJi <3dzhio7@gmail.com>
2026-05-15 23:24:56 +0200

    Update ACTIVATION.ps1

--- ACTIVATION.ps1
+++ ACTIVATION.ps1
@@ -1,17 +1,22 @@
-# GET ADMIN #
+### SELF-ELEVATE ###
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process "powershell" -ArgumentList "-ExecutionPolicy Bypass -NoProfile -File `"$PSCommandPath`"" -Verb RunAs
+    Start-Process -FilePath "powershell" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
 
-# VARIABLES #
-$WORK_DIR     = "$env:TEMP\ohook"
-$ZIP_PATH     = "$WORK_DIR\ohook.zip"
+
+
+### VARIABLES ###
+$WORK_DIR     = "$env:TEMP\OHOOK"
+$ZIP_PATH     = "$WORK_DIR\OHOOK.zip"
 $DOWNLOAD_URL = "https://github.com/asdcorp/ohook/releases/download/0.5/ohook_0.5.zip"
 $OFFICE_DIR   = "$env:PROGRAMFILES\Microsoft Office\root\vfs\System"
 $SYSTEM_DIR   = "$env:SYSTEMROOT\System32"
-$PRODUCT_KEY  = "VWCNX-7FKBD-FHJYG-XBR4B-88KC6" # ProPlus2024Retail ( https://massgrave.dev/manual_ohook_activation#office-2024 )
+$PRODUCT_KEY  = "VWCNX-7FKBD-FHJYG-XBR4B-88KC6" # ProPlus2024Retail ( https://massgrave.dev/manual_ohook_activation#office-2024 ) #
 
+
+
+### MAIN ###
 # STOP OFFICE #
 Get-CimInstance -ClassName "Win32_Process" | Where-Object { $_.ExecutablePath -match "Microsoft Office|ClickToRun" } | Invoke-CimMethod -MethodName "Terminate" -ErrorAction "SilentlyContinue"
 Start-Sleep -Seconds 2
